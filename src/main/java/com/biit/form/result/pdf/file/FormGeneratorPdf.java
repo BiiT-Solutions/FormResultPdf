1db56cc5f729e139dd20f04748009c0637a404ba
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-04-14 20:19:56 +0200

    Adding sonarqube.yaml

--- src/main/java/com/biit/form/result/pdf/file/FormGeneratorPdf.java
+++ src/main/java/com/biit/form/result/pdf/file/FormGeneratorPdf.java
@@ -1,33 +1,32 @@
 package com.biit.form.result.pdf.file;
 
+import com.biit.form.result.pdf.FormAsPdf;
+
 import java.io.ByteArrayInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
-
-import com.biit.form.result.pdf.FormAsPdf;
 
 /**
  * Class with utility methods
  */
 public class FormGeneratorPdf {
 
-	public static InputStream generatePdf(FormAsPdf generator) throws Exception {
-		if (generator != null) {
-			byte[] data = generator.generate();
-			// convert array of bytes into file
-			InputStream inputStream = new ByteArrayInputStream(data);
-			return inputStream;
-		}
-		return null;
-	}
+    public static InputStream generatePdf(FormAsPdf generator) throws Exception {
+        if (generator != null) {
+            byte[] data = generator.generate();
+            // convert array of bytes into file
+            return new ByteArrayInputStream(data);
+        }
+        return null;
+    }
 
-	public static void generatePdfAsFile(String filename, FormAsPdf generator) throws Exception {
-		if (generator != null) {
-			byte[] data = generator.generate();
-			// convert array of bytes into file
-			FileOutputStream fileOuputStream = new FileOutputStream(filename);
-			fileOuputStream.write(data);
-			fileOuputStream.close();
-		}
-	}
+    public static void generatePdfAsFile(String filename, FormAsPdf generator) throws Exception {
+        if (generator != null) {
+            byte[] data = generator.generate();
+            // convert array of bytes into file
+            try (FileOutputStream fileOuputStream = new FileOutputStream(filename)) {
+                fileOuputStream.write(data);
+            }
+        }
+    }
 }
